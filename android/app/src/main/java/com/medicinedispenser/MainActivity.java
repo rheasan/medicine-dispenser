5cf01080ce1ef299b0760c35ed79d37a937575a9
Shreyas Chavan <shreyaschavan228a@gmail.com>
2022-03-12 13:05:58 +0530

    added @react-navigation/native and dependencies

--- android/app/src/main/java/com/medicinedispenser/MainActivity.java
+++ android/app/src/main/java/com/medicinedispenser/MainActivity.java
@@ -1,6 +1,7 @@
 package com.medicinedispenser;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +9,13 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected String getMainComponentName() {
-    return "MedicineDispenser";
-  }
+    @Override
+    protected String getMainComponentName() {
+        return "MedicineDispenser";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+}
 }
