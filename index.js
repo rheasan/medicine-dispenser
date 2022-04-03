e623f8d67115758a5439c7792ebe95910c73d015
Shreyas Chavan <shreyaschavan228a@gmail.com>
2022-04-03 23:39:28 +0530

    removed unnecessary packages

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import PushNotification from 'react-native-push-notification';
+
+
 
 AppRegistry.registerComponent(appName, () => App);
+
+PushNotification.configure({
+    // (required) Called when a remote or local notification is opened or received
+    onNotification: function (notification) {
+        console.log('LOCAL NOTIFICATION ==>', notification)
+    },
+
+    popInitialNotification: true,
+    requestPermissions: true,
+});
